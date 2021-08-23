f0a90b8443d1de1a52ab51142d76be86ca4a665a
AnsonRaphael <anson550220@gmail.com>
2021-08-23 16:56:20 +0530

    DSA day: 23-08-2021 problems

---
+++ CC/src/com/nanos/dsa/arrays/FindFirstLastPositionElementSortedArray.java
@@ -0,0 +1,49 @@
+package com.nanos.dsa.arrays;
+
+import java.util.Arrays;
+
+public class FindFirstLastPositionElementSortedArray {
+    public static void main(String[] args) {
+        int[] nums={5,7,7,8,8,10};
+        int target=8;
+        System.out.println(Arrays.toString(searchRange(nums,target)));
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        int f= first(nums, target);
+        int l= last(nums, target);
+        int[] res=new int[2];
+        res[0]=f;
+        res[1]=l;
+        return res;
+    }
+    public static int first(int[] nums,int k){
+        int l=nums.length-1,f=0;
+        while(f<=l){
+            int mid=(l+f)/2;
+            if(nums[mid]==k&&(mid==0||nums[mid-1]!=k)){
+                return mid;
+            }
+            if(nums[mid]>=k){
+                l=mid-1;
+            }else{
+                f=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static int last(int[] nums,int k){
+        int l=nums.length-1,f=0;
+        while(f<=l){
+            int mid=(l+f)/2;
+            if(nums[mid]==k&&(mid==nums.length-1||nums[mid+1]!=k)){
+                return mid;
+            }
+            if(nums[mid]<=k){
+                f=mid+1;
+            }else{
+                l=mid-1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ CC/src/com/nanos/dsa/arrays/RotateArray.java
@@ -0,0 +1,29 @@
+package com.nanos.dsa.arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums={-1};
+        int k=2;
+        rotate(nums,k);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        k=nums.length-k;
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+        reverse(nums,0,nums.length-1);
+
+    }
+    public static void reverse(int[] nums,int s,int e){
+        while(s<=e){
+            int t=nums[s];
+            nums[s]=nums[e];
+            nums[e]=t;
+            s++;
+            e--;
+        }
+    }
+}
